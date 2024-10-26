01c1f6bca38877afd0ed9a0c5d3491c706bcb56f
Vivek <vvekkurhe555@gmail.com>
2024-10-26 12:14:33 +0530

    cypress learn

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,27 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress\\config', `${file}.json`)
+
+  if(!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found.")
+    return {}
+  }
+
+  return fs.readJson(pathToConfigFile)
+}
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || ''
+
+      return getConfigurationByFile(file)
     },
-    baseUrl: 'https://automationteststore.com',
+    baseUrl: 'https://webdriveruniversity.com',
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     excludeSpecPattern: "cypress/e2e/todo/*.js",
     chromeWebSecurity: false,
@@ -16,6 +32,11 @@ module.exports = defineConfig({
     reporter: 'cypress-multi-reporters',
     reporterOptions: {
       configFile: 'reporter-config.json',
+    },
+    // retries failing test for a specified no. of times
+    retries: {
+      runMode: 0,
+      openMode: 1
     },
     // env variable can also used to define different env URLs
     env: {
--- cypress/e2e/webdriver-uni/retries.js
+++ cypress/e2e/webdriver-uni/retries.js
@@ -1,12 +1,19 @@
 describe("test contact us form via webdriveruni", () => {
 
-    it("should be able to submit a succesful submission via contact us form", () => {
+    // if you are sure that this particular test is likely to fail in the first attempt, you can
+    // retry this particular test by passing retries as an argument.
+    it("should be able to submit a succesful submission via contact us form", {
+        retries: {
+            runMode: 2,
+            openMode: 2
+        }
+    }, () => {
         cy.visit("/Contact-Us/contactus.html")
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('include', 'WebDriver | Contact Us')
         cy.url().should('include', 'Contact-Us/contactus.html')
         // cy.get('#contact-us').click({force: true})
-        cy.get('[name=first_name]').type('Vivek')
+        cy.get('[name=first_name]555').type('Vivek')
         cy.get('[name=last_name]').type('Kurhe')
         cy.get('[name=email]').type('vivek.kurhe@gmail.com')
         cy.get('[name=message]').type('Hi Vivek, How are you doing?')
